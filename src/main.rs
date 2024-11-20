681dd198592820fbbb94f5dfaf80730251a92579
Hirotaka Inoue <91177804+HirotakaInoue@users.noreply.github.com>
2024-11-21 00:19:51 +0900

    add_sample_route

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,12 @@
-fn main() {
-    println!("Hello, world!");
+pub mod routes;
+
+#[tokio::main]
+async fn main() {
+    let app = routes::create_routes();
+    // サーバーの起動
+    let listener = tokio::net::TcpListener::bind("127.0.0.1:3000")
+        .await
+        .unwrap();
+    println!("Listening on: {}", listener.local_addr().unwrap());
+    axum::serve(listener, app).await.unwrap();
 }
