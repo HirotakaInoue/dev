4d7def310969586930623303c95590952f566b1b
Hirotaka Inoue <91177804+HirotakaInoue@users.noreply.github.com>
2024-11-24 01:40:11 +0900

    docker

--- server/src/main.rs
+++ server/src/main.rs
@@ -5,9 +5,7 @@ pub mod routes;
 async fn main() {
     let app = routes::create_routes();
     // サーバーの起動
-    let listener = tokio::net::TcpListener::bind("127.0.0.1:3000")
-        .await
-        .unwrap();
+    let listener = tokio::net::TcpListener::bind("0.0.0.0:3000").await.unwrap();
     println!("Listening on: {}", listener.local_addr().unwrap());
     axum::serve(listener, app).await.unwrap();
 }
